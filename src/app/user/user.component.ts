0305887b04147c77c70539f83bfc618b49fa6375
r.olive <r.olive@plugchain.energy>
2024-09-28 21:15:54 +0200

    Feat: Add user component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss',
+})
+export class UserComponent {
+  selectedUser = DUMMY_USERS[0];
+}
