5f4282c5cbed4fd155b9c36907f709727e4c8c2f
r.olive <r.olive@plugchain.energy>
2024-10-01 16:13:34 +0200

    Feat: Add new task

--- src/app/new-task/new-task.component.ts
+++ src/app/new-task/new-task.component.ts
@@ -1,16 +1,33 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { type NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.scss',
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskData>();
+  enteredTask: NewTaskData = {
+    title: '',
+    summary: '',
+    dueDate: '',
+  };
+  enteredTask1 = signal(''); // Alternative way to deal with two-way-binding on angular
 
   onCancel() {
     this.cancel.emit();
+  }
+
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTask.title,
+      summary: this.enteredTask.summary,
+      dueDate: this.enteredTask.dueDate,
+    });
   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
 import { dummyTasks } from '../dummy-tasks';
 import { HeaderComponent } from '../header/header.component';
 import { NewTaskComponent } from '../new-task/new-task.component';
 import { TaskComponent } from '../task/task.component';
+import { type NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -31,6 +33,17 @@ export class TasksComponent {
   }
 
   onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.unshift({
+      id: uuidv4(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.dueDate,
+    });
     this.isAddingTask = false;
   }
 }
